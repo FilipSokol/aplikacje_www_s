ca0b1e0ee0eff476ccb9cc2d13f33760603d5885
SebastianGonera <email@smiths.smiths>
2022-03-05 17:03:12 +0100

    Update lab1_1

---
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+namespace SchoolRegister.Model.DataModels
+{
+    public class Student : User
+    {
+        public SubjectGroup Group { get; set; }
+        public int? GroupId { get; set; }
+        public List<Grade> Grades { get; set; }
+        public Parent Parent { get; set; }
+        public int? ParentId { get; set; }
+        public double AverageGrade { get; }
+        public Dictionary<string, double> AverageGradePerSubject { get; }
+        public Dictionary<string, List<GradeScale>> GradePerSubject { get; }
+        public Student() { }
+    }
+
+}
