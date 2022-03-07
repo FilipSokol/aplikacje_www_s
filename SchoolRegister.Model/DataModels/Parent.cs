0c2e56447854a22c8a0a4d7b10ad2e9bc22c0f27
Dominik <janek@gmail.com>
2022-03-07 11:17:01 +0100

    lab2

---
+++ SchoolRegister.Model/DataModels/Parent.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+namespace SchoolRegister.Model.DataModels
+{
+    public class Parent
+    {
+        public List<Student> Students { get; set; }
+        public Parent() { }
+    }
+
+}
