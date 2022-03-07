e470c6507a75a415e87795498f879a9832f4d4d3
Marcel <aplikacjewwws_fake@smth.pl>
2022-03-07 11:23:41 +0100

    lab2

---
+++ SchoolRegister.model/DataModels/Parent.cs
@@ -0,0 +1,13 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Parent
+    {
+        public IList<Student> Students { get; set; }
+        Parent() { }
+
+    }
+}
