a8f3bf56fe332f4ede4ab44ef30e09c690148028
Dawid Kozłowski <tojestprawdziwygmailhehe@gmail.com>
2022-03-07 15:45:13 +0100

    Lab2.2

--- SchoolRegister.Model/DataModels/Group.cs
+++ SchoolRegister.Model/DataModels/Group.cs
@@ -1,12 +1,13 @@
 using System;
 
 namespace SchoolRegister.Model.DataModels
+
 {
     public class Group
     {
         public int Id { get; set; }
         public string Name {get; set; }
-        public IList<Student> Students { get; set; }
+      
 
     }   
 }
