181b80a68042e1eafbbafc076ba0091eb6513bce
Dominik <janek@gmail.com>
2022-03-07 11:41:19 +0100

    lab2

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -1,19 +1,35 @@
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+
 namespace SchoolRegister.Model.DataModels
 {
     public class Student : User
     {
-        public SubjectGroup Group { get; set; }
+        public Group Group { get; set; }
         public int? GroupId { get; set; }
-        public List<Grade> Grades { get; set; }
+        public IList<Grade> Grades { get; set; }
         public Parent Parent { get; set; }
         public int? ParentId { get; set; }
-        public double AverageGrade { get; }
-        public Dictionary<string, double> AverageGradePerSubject { get; }
-        public Dictionary<string, List<GradeScale>> GradePerSubject { get; }
-        public Student() { }
+        public double AverageGrade => Grades.Average(Grade => (int)Grade.GradeValue);
+        public IDictionary<string, double> AverageGradePerSubject
+        {
+            get
+            {
+                return Grades.GroupBy(grade => grade.Subject.Name)
+                             .Select(g => new { SubName = g.Key, Avg = g.Average(gv => (int)gv.GradeValue) })
+                             .ToDictionary(g => g.SubName, g => g.Avg);
+            }
+        }
+        public IDictionary<string, List<GradeScale>> GradesPerSubject
+        {
+            get
+            {
+                return Grades.GroupBy(grade => grade.Subject.Name)
+                             .Select(g => new { SubName = g.Key, GradeList = g.Select(gv => gv.GradeValue).ToList() })
+                             .ToDictionary(g => g.SubName, g => g.GradeList);
+            }
+        }
     }
-
 }
