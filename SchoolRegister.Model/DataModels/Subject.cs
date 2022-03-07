4158df7252ee435f761c544c49a7dad10b413e7a
MaskymZ <Starkiller2444@ukr.net>
2022-03-07 15:34:39 +0100

    Update

---
+++ SchoolRegister.Model/DataModels/Subject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Subject
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public Teacher teacher { get; set; }
+        public int teacherid { get; set; }
+        
+    }
+}
---
+++ SchoolRegister.Model/DataModels/SubjectGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class SubjectGroup
+    {
+        public Subject subject { get; set; }
+        public int subjectId { get; set; }
+        public int groupId { get; set; }
+
+    }
+}
