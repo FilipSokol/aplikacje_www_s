0362cf4240ffad8c99cd753885df43d8174c9169
Kamil <email@smth.smth>
2022-03-07 11:41:44 +0100

    final lab1

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -1,18 +1,35 @@
 using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SchoolRegister.Model.DataModels
 {
     public class Student : User
     {
-        public double AverageGrade{ get; }
-        public IDictionary<string, double> AverageGradePerSubject{ get; }
+        public Group Group { get; set; }
+        public int? GroupId { get; set; }
         public IList<Grade> Grades { get; set; }
-        public IDictionary<string, List<GradeScale>> GradesPerSubject{ get; }
-        public Group Group { get; set;}
-        public int? GroupId {get; set; }
-        public Parent Parent {get; set;}
-        public int? ParentId {get; set; }
+        public Parent Parent { get; set; }
+        public int? ParentId { get; set; }
+        public double AverageGrade => Grades.Average(Grade => (int)Grade.GradeValue);
+        public IDictionary<string, double> AverageGradePerSubject
+        {
+            get
+            {
+                return Grades.GroupBy(grade => grade.Subject.Name)
+                             .Select(g => new { SubName = g.Key, Avg = g.Average(gv => (int)gv.GradeValue) })
+                             .ToDictionary(g => g.SubName, g => g.Avg);
+            }
+        }
+        public IDictionary<string, List<GradeScale>> GradesPerSubject
+        {
+            get
+            {
+                return Grades.GroupBy(grade => grade.Subject.Name)
+                             .Select(g => new { SubName = g.Key, GradeList = g.Select(gv => gv.GradeValue).ToList() })
+                             .ToDictionary(g => g.SubName, g => g.GradeList);
+            }
+        }
     }
 }
