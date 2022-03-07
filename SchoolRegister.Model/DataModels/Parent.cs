61a209731997b6fa920bc023195d40f2c65f0051
Sebastian <mail@aser.ert>
2022-03-07 11:13:28 +0100

    Laboratorium 2

---
+++ SchoolRegister.Model/DataModels/Parent.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Parent : User
+    {
+        public IList<Student> Students { get; set; }
+        public Parent() { }
+    }
+}
--- SchoolRegister.Model/DataModels/Teacher.cs
+++ SchoolRegister.Model/DataModels/Teacher.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Identity;
-using System.Coll
+using System.Collections.Generic;
 using System;
 
 namespace SchoolRegister.Model.DataModels
@@ -8,6 +8,7 @@ namespace SchoolRegister.Model.DataModels
     {
         public IList<Subject> Subjects { get; set; }
         public string Title { get; set; }
-        public void Teacher();
+        public Teacher() { }
     }
+
 }
